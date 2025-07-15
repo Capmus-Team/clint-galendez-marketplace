03b5f7710fcb5191407ba2dde923f769529a749b
clintgalendez <galendez.clintjonathan@gmail.com>
2025-07-16 06:21:19 +0800

    Add profile module exports for service and types

---
+++ lib/profile/index.ts
@@ -0,0 +1,8 @@
+export { ProfileService } from './profile-service'
+export { StorageUtils } from './storage-utils'
+export type { 
+  UserProfile, 
+  ProfileUpdateData, 
+  ProfileFormData, 
+  AvatarUploadResult 
+} from './types'
