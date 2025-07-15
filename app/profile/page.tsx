f9131a385ae923e33ed1864696ae849f3a979d50
clintgalendez <galendez.clintjonathan@gmail.com>
2025-07-16 03:25:50 +0800

    Refactor ProfilePage layout by removing ProfileStats component for a cleaner design

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -2,7 +2,6 @@
 
 import { ProfileHeader } from "@/components/profile/profile-header"
 import { ProfileInfo } from "@/components/profile/profile-info"
-import { ProfileStats } from "@/components/profile/profile-stats"
 import { ProtectedRoute } from "@/components/auth/protected-route"
 import { StripeAccountSetup } from "@/components/stripe/stripe-account-setup"
 
@@ -12,14 +11,9 @@ export default function ProfilePage() {
       <div className="min-h-screen bg-gradient-to-br from-blue-50 via-indigo-50 to-purple-50">
         <ProfileHeader />
         <div className="container mx-auto px-4 py-6 max-w-4xl">
-          <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
-            <div className="lg:col-span-2 space-y-6">
-              <ProfileInfo />
-              <StripeAccountSetup />
-            </div>
-            <div className="lg:col-span-1">
-              <ProfileStats />
-            </div>
+          <div className="lg:col-span-2 space-y-6">
+            <ProfileInfo />
+            <StripeAccountSetup />
           </div>
         </div>
       </div>
