b27f95ee7804e0325fb9eb1a821c75f05b332512
clintgalendez <galendez.clintjonathan@gmail.com>
2025-07-16 02:23:53 +0800

    Add ApplicationFeeWebhook export to StripeService aggregator

--- lib/stripe/index.ts
+++ lib/stripe/index.ts
@@ -4,19 +4,22 @@ export * from './database';
 export * from './account-service';
 export * from './product-service';
 export * from './payment-service';
+export * from './application-fee-webhook';
 export * from './hooks';
-export * from './client';
+export { getStripe as getStripeClient } from './client';
 
 // Main Stripe service aggregator
 import { StripeAccountService } from './account-service';
 import { StripeProductService } from './product-service';
 import { StripePaymentService } from './payment-service';
+import { ApplicationFeeWebhookService } from './application-fee-webhook';
 import { StripeDatabase } from './database';
 
 export const StripeService = {
   Account: StripeAccountService,
   Product: StripeProductService,
   Payment: StripePaymentService,
+  ApplicationFeeWebhook: ApplicationFeeWebhookService,
   Database: StripeDatabase,
 } as const;
 
